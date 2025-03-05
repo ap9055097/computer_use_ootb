aa4637d548823f824544ded876c17765d533c7de
Peerapat Kaewdee <ap09055097@gmail.com>
2025-03-05 13:24:53 +0700

    Update

--- app.py
+++ app.py
@@ -716,6 +716,6 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
     
 
 demo.launch(
-            # share=True,
+            share=True,
             allowed_paths=["./"],
             server_port=7888)  # TODO: allowed_paths
