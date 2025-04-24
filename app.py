c8cc5d75af8cdac0e145d3d65b9ef0149e1ad135
Peerapat Kaewdee <ap09055097@gmail.com>
2025-04-24 21:10:44 +0700

    Update

--- app.py
+++ app.py
@@ -852,12 +852,20 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
     
     
 
-demo.launch(
+# demo.launch(
+#             share=True,
+#             # share=False,
+#             # prevent_thread_lock=True,
+#             allowed_paths=["./"],
+#             server_port=7888)  # TODO: allowed_paths
+
+queue = demo.queue()
+_, _, tunnel_adress = queue.launch(
             share=True,
             # share=False,
-            # prevent_thread_lock=True,
+            prevent_thread_lock=True,
             allowed_paths=["./"],
             server_port=7888)  # TODO: allowed_paths
-
-
+print(tunnel_adress)
+queue.block_thread()
 
