c8fb478a9222167c5425d21e13c5096dc32dcc71
Peerapat Kaewdee <ap09055097@gmail.com>
2025-04-25 19:46:12 +0700

    update

--- run_keep_session.bat
+++ run_keep_session.bat
@@ -3,3 +3,5 @@ rem Redirect active RDP session to console
 for /f "skip=1 tokens=3" %%s in ('query user %USERNAME%') do (
   %windir%\System32\tscon.exe %%s /dest:console
 )
+timeout /t 5
+qres /x 1440 /y 900 /c 32
